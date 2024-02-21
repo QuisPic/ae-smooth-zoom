e5b42bc91eff3e0019a183587aee793c141fb665
Denis Mozgovoi <denis.quis@gmail.com>
2024-02-21 21:27:26 +0100

    improve text descriptions for experimental settings

--- src/ui/settings/experimental-settings.js
+++ src/ui/settings/experimental-settings.js
@@ -4,6 +4,7 @@ import JSON from "../../../extern/json2";
 import zoomPlugin from "../../zoomPlugin";
 import PluginStatusWithButton from "../status/plugin-status-with-button";
 import { ZOOM_PLUGIN_STATUS } from "../../constants";
+import bind from "../../../extern/function-bind";
 
 function ExperimentalSettings(parentEl) {
   this.element = parentEl.add(
@@ -24,11 +25,11 @@ function ExperimentalSettings(parentEl) {
           grCheck: Group { \
             spacing: 2, \
             chk: Checkbox { preferredSize: [-1, 14] }, \
-            txt: StaticText { text: 'Zoom only if cursor is inside a viewport' }, \
+            txt: StaticText { text: 'Change zoom only when cursor is inside a viewport' }, \
           }, \
           txtDescription: StaticText { \
-            text: 'If enabled, Zoom keybindings that include mouse events will be captured only when the cursor is inside a viewport.', \
-            characters: 50, \
+            text: 'If enabled, Zoom key bindings that include mouse events will be captured only when the mouse cursor is inside a viewport. If multiple viewports are open, Zoom will target the viewport currently under the mouse cursor.', \
+            characters: 55, \
             properties: { multiline: true }, \
           },\
         }, \
@@ -38,20 +39,25 @@ function ExperimentalSettings(parentEl) {
           grCheck: Group { \
             spacing: 2, \
             chk: Checkbox { preferredSize: [-1, 14] }, \
-            txt: StaticText { text: 'Maintain view position' }, \
+            txt: StaticText { text: 'Maintain View Position' }, \
           }, \
           txtDescription: StaticText {\
-            text: 'If enabled, the composition view will maintain its position relative to the point selected above after zooming. If disabled, the view will always be centered after zooming.', \
+            text: 'If enabled, the composition view will maintain its position after zooming.', \
             characters: 50, \
-            properties: { multiline: true }, \
+            properties: { multiline: false }, \
           },\
-          grZoomAround: Group { \
-            txtZoomAround: StaticText {\
-              text: 'Change zoom relative to: ', \
-            }, \
-            ddlistZoomPoint: DropDownList { \
-              properties: { \
-                items: ['View Panel Center (AE default)', 'Cursor Position'],\
+          pnlZoomAround: Panel { \
+            alignment: 'fill', \
+            alignChildren: 'left', \
+            text: 'Mouse Wheel', \
+            grZoomAround: Group { \
+              txtZoomAround: StaticText {\
+                text: 'When using mouse wheel, change zoom relative to: ', \
+              }, \
+              ddlistZoomPoint: DropDownList { \
+                properties: { \
+                  items: ['View Panel Center (AE default)', 'Cursor Position'],\
+                }, \
               }, \
             }, \
           }, \
@@ -78,12 +84,19 @@ function ExperimentalSettings(parentEl) {
     }
   });
 
+  fixViewportPositionChkGr.chk.onClick = bind(function () {
+    var pnlFixViewportPosition = this.element.gr.pnlFixViewportPosition;
+
+    pnlFixViewportPosition.pnlZoomAround.enabled =
+      pnlFixViewportPosition.grCheck.chk.value;
+  }, this);
+
   detectCursorInsideViewChkGr.chk.value =
     experimentalPrefs.detectCursorInsideView;
 
   fixViewportPositionChkGr.chk.value =
     experimentalPrefs.fixViewportPosition.enabled;
-  this.element.gr.pnlFixViewportPosition.grZoomAround.ddlistZoomPoint.selection =
+  this.element.gr.pnlFixViewportPosition.pnlZoomAround.grZoomAround.ddlistZoomPoint.selection =
     experimentalPrefs.fixViewportPosition.zoomAround;
 
   /** Show plug-in status if plug-in is not found */
